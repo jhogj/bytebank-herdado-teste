04be7b4b92b1e8f6e739bf3c10ec45e4df3ad83f
José Hudson de Oliveira Guimarães Junior <102635510+jhogj@users.noreply.github.com>
2022-05-20 13:58:29 -0300

    Update Autenticavel.java

--- src/Autenticavel.java
+++ src/Autenticavel.java
@@ -1,10 +1,3 @@
-//contrato autenticavel
-	//quem assinar esse contrato, precisa implementar
-		//metodo set senha
-		//metodo autentica
-		//agora essa classe não pode ser extends, e sim apenas implements
-
-
 public abstract interface Autenticavel {
 
 	public abstract void setSenha (int senha);
