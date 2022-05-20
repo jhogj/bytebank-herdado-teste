9e4ca8951113638b4f3347f97f3722cb25e9300c
José Hudson de Oliveira Guimarães Junior <102635510+jhogj@users.noreply.github.com>
2022-05-20 14:00:03 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,18 +1,12 @@
 
-public abstract class Funcionario { //abstract ta relacionado a herança
-									//o funcionário é pois é a mãe das profissões
-									//definidas aqui
-								//não se pode instanciar uma classe abstrata
+public abstract class Funcionario { 
 	
 	private String nome;
 	private String cpf;
-	private double salario; //pode se usar protected ao inves
-							// de private ou public
-							//protected = publico para os filhos
+	private double salario; 
 	
 	
-	//quando vc coloca abstract na classe mãe, vc obriga os filhos a implementarem 
-	//esse método
+	
 	public abstract double getBonificacao(); 
 	
 	public String getNome() {
